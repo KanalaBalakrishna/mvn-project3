4d814cd0ca72404cd430e18e928077e7135b1d01
Bala <br70403@gmail.com>
2023-02-20 17:33:24 +0530

    project3 commit

---
+++ src/test/java/group1/acceptancetests/DryRunTests.java
@@ -0,0 +1,11 @@
+package group1.acceptancetests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(glue = { "group1" }, features = "src/test/resources/features", plugin = { "json:target/cucumber-reports/cucumber.json" }, tags = { "@complete,@accepted,@dev,@wip" })
+public class DryRunTests {
+}
